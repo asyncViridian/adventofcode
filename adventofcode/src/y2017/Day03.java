80fe26c88f0d104e9e743f4cc77ec128f4f9b41a
asyncViridian <jyzhou15@uw.edu>
2017-12-20 22:36:23 -0800

    Day 3 part 1

--- adventofcode/src/y2017/Day03.java
+++ adventofcode/src/y2017/Day03.java
@@ -5,10 +5,26 @@ import java.util.*;
 
 public class Day03 {
 
-	public static final String INPUT = "";
+	public static final int INPUT = 347991;
+	public static final int NUM_CORNERS = 4;
 
 	public static void part1() {
-
+		int diagonalRoot = 1;
+		while ((diagonalRoot * diagonalRoot) < INPUT) {
+			diagonalRoot += 2;
+		}
+		int minCornerDistance = diagonalRoot;
+		for (int i = (NUM_CORNERS - 1); (i >= 0) && (minCornerDistance == diagonalRoot); i--) {
+			int testing = (diagonalRoot * diagonalRoot) - (i * (diagonalRoot - 1));
+			if (testing >= INPUT) {
+				int downDist = testing - INPUT;
+				int upDist = diagonalRoot - 1 - downDist;
+				minCornerDistance = (downDist < minCornerDistance) ? downDist : minCornerDistance;
+				minCornerDistance = (upDist < minCornerDistance) ? upDist : minCornerDistance;
+			}
+		}
+		int steps = (diagonalRoot - 1) - minCornerDistance;
+		System.out.println(steps);
 	}
 
 	public static void part2() {
