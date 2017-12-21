bd45b5426e0ad97afcc66c19e5518774d504edd2
asyncViridian <jyzhou15@uw.edu>
2017-12-20 22:04:59 -0800

    Init day 3

---
+++ adventofcode/src/y2017/Day03.java
@@ -0,0 +1,23 @@
+
+package y2017;
+
+import java.util.*;
+
+public class Day03 {
+
+	public static final String INPUT = "";
+
+	public static void part1() {
+
+	}
+
+	public static void part2() {
+
+	}
+
+	public static void main(String[] args) {
+		part1();
+		part2();
+	}
+
+}
